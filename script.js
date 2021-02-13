08870969027c616d6e10e7a6bf1f83e6af539adc
ThePotatoPowers <44983465+ThePotatoPowers@users.noreply.github.com>
2021-02-13 18:34:50 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Hello, World!");
+alert("Fortnite");
+const interestRate = 0.3;
+let testObject = {
+    name: "Fortnite",
+    age: 30
+};
+console.log(testObject.name)
+function greet(name) {
+console.log(`Hello ${name}`)
+};
+greet("test");
