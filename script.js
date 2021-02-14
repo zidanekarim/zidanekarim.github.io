b44877e2726b9c0bce60e9c430184140c3f1a134
ThePotatoPowers <44983465+ThePotatoPowers@users.noreply.github.com>
2021-02-13 19:19:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-console.log("Hello, World!");
 alert("Fortnite");
-const interestRate = 0.3;
-let testObject = {
-    name: "Fortnite",
-    age: 30
-};
-console.log(testObject.name)
-function greet(name) {
-console.log(`Hello ${name}`)
-};
-greet("test");
+function displayResult() {
+    document.getElementById("something").innerHTML = Date();
+  }
