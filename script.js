574dcbe2b647edf349efbeee120fd94ce086e5dd
ThePotatoPowers <44983465+ThePotatoPowers@users.noreply.github.com>
2021-02-16 11:34:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 alert("Fortnite");
 function displayResult() {
-    document.getElementById("something").innerHTML = Date();
-  }
+    document.getElementById("something").innerHTML = Date()
+};
+
+var click = 0;
+
+
+
+function cpsTest() {
+    click++;
+    document.getElementById("something").innerHTML = click;
+    setTimeout(function() {cps = click / 10; 
+    document.getElementById("totalclicks")(`You have ${cps} clicks per second! Noob!`); 
+    document.getElementById("something").innerHTML = "Click the button to play again!"}, 10000)
+    }
+
+
